f94fba641f724c3626cf3243be01af9fabe0b3da
lpow100 <128748375+lpow100@users.noreply.github.com>
2023-11-03 19:18:56 +0000

    added entering username and password

--- main.py
+++ main.py
@@ -2,38 +2,91 @@ import curses
 import os
 import pickle
 
-lines = ["─","│","┌","┐","└","┘"]
+chars = """abcdefghijklmonpqrstuvwxyzABCDEFGHIJKLMONPQRSTUVWXYZ1234567890!@#$%^&*()`~-=_+[]{}\|;:'",<.>/? \t"""
+lines = ["─", "│", "┌", "┐", "└", "┘"]
 
-def addcenteredstr(window,y:int,x:int,text:str) -> int:
-    """adds a centered string to the sepicfied win at x and y. returns the centered x"""
-    x -= len(text)//2
-    window.addstr(y,x,text)
+
+def add_centered_str(window, y, x, text):
+    x -= len(text) // 2
+    window.addstr(y, x, text)
+    window.border(0, 0, 0, 0, 0, 0, 0, 0)
     return x
- 
-def getstr(win,x,y):
-    key = " "
-    currentstr = ""
-    win.move(x,y)
-    while not (ord(key) in (10,13)): #enter or return
-        next_key = chr(win.getch())
-        key = key if next_key == -1 else next_key
-        if key == chr(127): #backspace
-            currentstr = currentstr[:-1]
+
+
+def get_written_str(win, x, y):
+    current_str = ""
+    while True:
+        key = win.getch()
+        if key == 127:  # backspace
+            if len(current_str) > 0:
+                current_str = current_str[:-1]
+                y -= 1
+                win.delch(x, y)
+                win.addstr(x, y, "  ")
+                win.delch(x, 58)
+        elif key in (10, 13):  # enter or return
+            break
         else:
-            currentstr += key
-    return currentstr
+            char = chr(key)
+            if char in chars:
+                current_str += char
+                y += 1
+                if len(current_str) < 30:
+                    win.addch(x, y - 1, char)
+        win.border(0, 0, 0, 0, 0, 0, 0, 0)
+    return current_str
+
+
+def get_dotted_str(win, x, y):
+    current_str = ""
+    while True:
+        key = win.getch()
+        if key == 127:  # backspace
+            if len(current_str) > 0:
+                current_str = current_str[:-1]
+                y -= 1
+                win.delch(x, y)
+                if len(current_str) > 1:
+                    win.delch(x, y - 1)
+                    win.addch(x, y - 1, current_str[-1])
+                    win.delch(x, 58)
+        elif key in (10, 13):  # enter or return
+            break
+        else:
+            char = chr(key)
+            if char in chars:
+                current_str += char
+                y += 1
+                if len(current_str) < 30:
+                    win.addch(x, y - 1, char)
+                win.addch(x, y - 2, "*")
+        win.border(0, 0, 0, 0, 0, 0, 0, 0)
+    return current_str
+
 
 def main():
     curses.initscr()
-    win = curses.newwin(20,60,0,0)
+    win = curses.newwin(20, 60, 0, 0)
     win.keypad(1)
+    curses.noecho()
     curses.curs_set(0)
     win.border(0, 0, 0, 0, 0, 0, 0, 0)
 
-    centeredx = addcenteredstr(win,10,30,"enter your username")
-    name = getstr(win,11,centeredx)
+    centered_x = add_centered_str(win, 10, 30, "Enter your username")
+    win.addstr(12, centered_x, "Enter your password")
+    name = get_written_str(win, 11, centered_x)
+    password = get_dotted_str(win, 13, centered_x)
+    #try:
 
     curses.endwin()
 
+def save(word_list,filename = 'mypickle.pk'):
+    with open(filename, 'wb') as fi:
+        pickle.dump(word_list, fi)
+
+def load(filename = 'mypickle.pk'):
+    with open(filename, 'rb') as fi:
+        return pickle.load(fi)
+
 if __name__ == "__main__":
-   main()
+    main()
